cd8eb40fafeafde4880e7d9afcf13a3821097557
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-12-07 14:29:53 +0000

    Add bind client to get server stats
    Co-authored-by: jbevan4 <joshua.bevan@madetech.com>
    Co-authored-by: Caitlin <caitlin@madetech.com>
    Co-authored-by: Efua <efua.akumanyi@gmail.com>

---
+++ dns-service/metrics/spec/bind_client_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'spec_helper'
+
+describe BindClient do
+  before do
+    stub_request(:get, "localhost:8080/json/v1/server").to_return(status: 200, body: "{\"json-stats-version\": \"1.5\"}", headers: {})
+  end
+
+  it "get basic server stats" do
+    expect(described_class.new.get_server_stats).to eq({"json-stats-version" => "1.5"})
+  end
+end
