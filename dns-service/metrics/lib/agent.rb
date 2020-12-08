0b442b27c4a1b90aaf9916908edeb2386b5f52d9
cait <caitlin@madetech.com>
2020-12-08 11:39:54 +0000

    Rename bind_stats to server_stats

--- dns-service/metrics/lib/agent.rb
+++ dns-service/metrics/lib/agent.rb
@@ -1,11 +1,11 @@
 require_relative "dns_metrics"
 
 while true
-  bind_stats = BindClient.new.get_server_stats
   puts "before metrics"
+  server_stats = BindClient.new.get_server_stats
   PublishMetrics.new(
     client: AwsClient.new
-  ).execute(bind_stats: bind_stats)
+  ).execute(server_stats: server_stats)
   puts "after metrics"
   sleep 10
 end
--- dns-service/metrics/lib/publish_metrics.rb
+++ dns-service/metrics/lib/publish_metrics.rb
@@ -6,18 +6,18 @@ class PublishMetrics
     @time = DateTime.now.to_time.to_i
   end
 
-  def execute(bind_stats:)
-    raise "BIND stats are empty" if bind_stats.empty?
+  def execute(server_stats:)
+    raise "BIND stats are empty" if server_stats.empty?
 
-    client.put_metric_data(generate_cloudwatch_metrics(bind_stats))
+    client.put_metric_data(generate_cloudwatch_metrics(server_stats))
   end
 
   private
 
-  def generate_cloudwatch_metrics(bind_stats)
-    return [] unless bind_stats.has_key?("nsstats")
+  def generate_cloudwatch_metrics(server_stats)
+    return [] unless server_stats.has_key?("nsstats")
 
-    bind_stats["nsstats"].map { |key, value| generate_metric(key, value) }.compact
+    server_stats["nsstats"].map { |key, value| generate_metric(key, value) }.compact
   end
 
   def generate_metric(key, value)
