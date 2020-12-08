f5bb5d3d2cd0122802e520122ba72260ee8f3774
cait <caitlin@madetech.com>
2020-12-08 12:56:38 +0000

    Pass in zone stats to the metrics agent

--- dns-service/metrics/lib/agent.rb
+++ dns-service/metrics/lib/agent.rb
@@ -3,9 +3,10 @@ require_relative "dns_metrics"
 while true
   puts "before metrics"
   server_stats = BindClient.new.get_server_stats
+  zone_stats = BindClient.new.get_zone_stats
   PublishMetrics.new(
     client: AwsClient.new
-  ).execute(server_stats: server_stats)
+  ).execute(server_stats: server_stats, zone_stats: zone_stats)
   puts "after metrics"
   sleep 10
 end
