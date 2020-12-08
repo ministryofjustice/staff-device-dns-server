954494578acfbd174c26b86e86073601cc8d56a7
cait <caitlin@madetech.com>
2020-12-08 12:51:36 +0000

    Add get_zone_stats to BindClient

--- dns-service/metrics/spec/bind_client_spec.rb
+++ dns-service/metrics/spec/bind_client_spec.rb
@@ -1,11 +1,35 @@
 require_relative "spec_helper"
 
 describe BindClient do
-  before do
-    stub_request(:get, "localhost:8080/json/v1/server").to_return(status: 200, body: "{\"json-stats-version\": \"1.5\"}", headers: {})
+  describe "#get_server_stats" do
+    before do
+      stub_request(:get, "localhost:8080/json/v1/server").to_return(status: 200, body: "{\"json-stats-version\": \"1.5\"}", headers: {})
+    end
+
+    it "get basic server stats" do
+      expect(described_class.new.get_server_stats).to eq({"json-stats-version" => "1.5"})
+    end
   end
 
-  it "get basic server stats" do
-    expect(described_class.new.get_server_stats).to eq({"json-stats-version" => "1.5"})
+  describe "#get_zone_stats" do
+    before do
+      zone_body = "{\"views\":{\"_default\":{\"zones\":[{\"name\":\"test1.example.com\"},{\"name\":\"test2.localhost\"}]}}}"
+      stub_request(:get, "localhost:8080/json/v1/zones").to_return(status: 200, body: zone_body, headers: {})
+    end
+
+    it "get BIND zone stats" do
+      expect(described_class.new.get_zone_stats).to eq(
+        {
+          "views" => {
+            "_default" => {
+              "zones" => [
+                {"name"=>"test1.example.com"},
+                {"name"=>"test2.localhost"}
+              ]
+            }
+          }
+        }
+      )
+    end
   end
 end
