bce2f4a37b634fef179e0d7df57ed3846be45cf7
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-12-08 16:48:21 +0000

    Daemonise metrics agent and wait for DNS server to start before publishing
    We found daemonising the DNS server to be too difficult so instead we are
    daemonising the metrics agent and running it before. As we are running the agent
    in the background, we need to add a check to make sure the server is up before
    we start publishing metrics (otherwise the agent will throw an exception and stop)
    
    Co-authored-by: Efua Akumanyi <efua.akumanyi@gmail.com>

--- dns-service/metrics/lib/agent.rb
+++ dns-service/metrics/lib/agent.rb
@@ -1,5 +1,15 @@
 require_relative "dns_metrics"
 
+# Wait for DNS server to start
+10.times do
+  BindClient.new.get_server_stats
+rescue Errno::EADDRNOTAVAIL => e
+  sleep 1
+  next # Server is still booting so try again
+else
+  break # Server is up so we can continue
+end
+
 while true
   puts "before metrics"
   server_stats = BindClient.new.get_server_stats
