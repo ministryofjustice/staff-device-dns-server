f38ebcee7564448338a3ab4c0a51e222f594cd91
Jiv Dhaliwal <jiv.dhaliwal@madetech.com>
2020-12-09 14:32:12 +0000

    Extract API checking code to its own class
    Allows us to add tests around the API checking logic and makes the agent as
    simple as possible

--- dns-service/metrics/lib/agent.rb
+++ dns-service/metrics/lib/agent.rb
@@ -1,16 +1,8 @@
 require_relative "dns_metrics"
 
-# Wait for DNS server to start
-10.times do
-  BindClient.new.get_server_stats
-rescue Errno::EADDRNOTAVAIL => e
-  sleep 1
-  next # Server is still booting so try again
-else
-  break # Server is up so we can continue
-end
+ApiChecker.new.execute
 
-while true
+loop do
   PublishMetrics.new(
     aws_client: AwsClient.new,
     bind_client: BindClient.new
