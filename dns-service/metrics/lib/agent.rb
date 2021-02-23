de25b262734abc921bc8191628f3bbd4c40d76d4
Andy Mitchell <me@themitchell.co.uk>
2021-02-23 11:36:06 +0000

    Add sentry error reporting

--- dns-service/metrics/lib/agent.rb
+++ dns-service/metrics/lib/agent.rb
@@ -1,11 +1,47 @@
+require "sentry-ruby"
 require_relative "dns_metrics"
 
-ApiChecker.new.execute
-
-loop do
-  PublishMetrics.new(
-    aws_client: AwsClient.new,
-    bind_client: BindClient.new
-  ).execute
-  sleep 10
+Sentry.init do |config|
+  # All errors will be sent syncronously!
+  config.background_worker_threads = 0
+  config.environment = ENV["RUBY_ENV"]
 end
+
+class Agent
+  def execute
+    begin
+      api_checker.execute
+    rescue => e
+      Sentry.capture_exception(e)
+      raise e
+    end
+
+    loop do
+      begin
+        PublishMetrics.new(
+          aws_client: aws_client,
+          bind_client: bind_client
+        ).execute
+        sleep 10
+      rescue => e
+        Sentry.capture_exception(e)
+      end
+    end
+  end
+
+  private
+
+  def api_checker
+    @api_checker ||= ApiChecker.new
+  end
+
+  def aws_client
+    @aws_client ||= AwsClient.new
+  end
+
+  def bind_client
+    @bind_client ||= BindClient.new
+  end
+end
+
+Agent.new.execute
