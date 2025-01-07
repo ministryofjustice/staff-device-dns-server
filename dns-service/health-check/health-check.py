3bd1b5406313c782895505caa87cd76d125c7276
James Green <james.green6@justice.gov.uk>
2025-01-07 15:41:55 +0000

    print health check errors to stdout in the event of failer

--- dns-service/health-check/health-check.py
+++ dns-service/health-check/health-check.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
 import dns.resolver
 import os
+import sys
 
+
+sys.stdout = sys.stderr
 app = Flask(__name__)
 
 @app.route("/health")
@@ -11,9 +14,12 @@ def health_check():
         resolver.nameservers = ['127.0.0.1']  # Local BIND DNS server
         answer = resolver.query(os.environ['DNS_HEALTH_CHECK_URL'], "A")
         if answer:
-            return jsonify({"status": "healthy", "result": str(answer.rrset)}), 200
+            status = {"status": "healthy", "result": str(answer.rrset)}
+            return jsonify(status), 200
     except Exception as e:
-        return jsonify({"status": "unhealthy", "error": str(e)}), 503
+        error_status = {"status": "unhealthy", "error": str(e)}
+        print(f"Health Check Failed: {error_status}")
+        return jsonify(error_status), 503
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
