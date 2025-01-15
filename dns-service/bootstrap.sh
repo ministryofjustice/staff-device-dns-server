06a8bdd10d31ff43dd501ac31ebeb2b35b46d619
lauren_tb <lauren.taylor-brown@justice.gov.uk>
2025-01-15 14:17:24 +0000

    amended test conf and bootstrap script to enable debug logging

--- dns-service/bootstrap.sh
+++ dns-service/bootstrap.sh
@@ -5,13 +5,12 @@ set -e
 fetch_bind_config_file() {
   if [ "$LOCAL_DEVELOPMENT" == "true" ]; then
     cp ./named.conf /etc/bind/named.conf
-  else
-    aws s3 cp s3://${BIND_CONFIG_BUCKET_NAME}/named.conf /etc/bind/named.conf
+
   fi
 }
 
 start_dns_server() {
-  /usr/sbin/named -f -g
+  /usr/sbin/named -f 2>&1
 }
 
 start_dns_server_health_check() {
